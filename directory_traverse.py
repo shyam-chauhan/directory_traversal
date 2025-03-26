e0d76d21aa27a0b6b7f233df221fcd5bce007cf9
Chauhan Shyam <59696796+shyam-chauhan@users.noreply.github.com>
2025-03-26 10:53:29 +0530

    Add files via upload

--- directory_traverse.py
+++ directory_traverse.py
@@ -1,31 +1,31 @@
-#!/usr/bin/python
+#!/usr/bin/python3.8
 
 import requests
 from colorama import Fore
 import argparse
+from tqdm import tqdm
+import os
 
-def file_reader():
-    paths = open('paths.txt','r')
-    return paths
 
 def brute(url,file_path):
     hits = []
-    paths = file_reader()
-    for path in paths:
-        path = path.strip()
-        full_path = path.format(FILE=file_path)
-        full_url = url + full_path
-        res_code = requests.get(full_url)
-        print(Fore.MAGENTA + "Trying URL path : ",full_url)
-        if(res_code.status_code == 200):
-            hits.append(full_url)
-            print(Fore.GREEN + "hit on URL : ",full_url)
+    with open('paths.txt','r') as f:
+        paths = f.readlines()
+        lines = len(paths)
+        for path in tqdm(paths, total= lines,  dynamic_ncols=True, leave=False):
+            path = path.strip()
+            full_path = path.format(FILE=file_path)
+            full_url = url + full_path
+            res_code = requests.get(full_url)
+            if(res_code.status_code == 200):
+                hits.append(full_url)
+                print(Fore.GREEN + "\nhit {status} on URL : {url}".format(status = res_code.status_code,url = full_url))
     return hits          
 
 
 
 def main():
-    parser = argparse.ArgumentParser(description="Directory Travarsal \n exaple : python directory_traverse.py --url https://exaple.com/ --path /etc/passwd")
+    parser = argparse.ArgumentParser(description="Directory Travarsal \n exaple : python directory_traverse.py --url https://exaple.com/ --path etc/passwd")
 
     g1 = parser.add_argument_group("Group 1", "URL and path getter")
 
@@ -34,11 +34,11 @@ def main():
     g1.add_argument("--path",type=str, help="provide path for brute force e.g. etc/passwd")
 
     args = parser.parse_args()
-    print(args.url,args.path)
 	
     if args.url and args.path:
         hits = brute(args.url,args.path)
-        print(Fore.GREEN + str(hits))
+        print (Fore.BLUE + "ALL URLS :")
+        print(str(hits))
     else:
         print("Please provide url and path both")
 	
